7c3983f831d6c22eb4ebb3c8c084df5294636236
Andrew Larkin <andrewlarkin2@gmail.com>
2012-11-05 16:08:17 -0500

    Rearranging directory structure, adding requirejs dependencies

---
+++ app.build.js
@@ -0,0 +1,16 @@
+({
+    baseUrl: "lib",
+    dir: "build",
+    paths: {
+    	xui: 'ui/xui',
+    	jquery: "vendor/jquery",
+    	base: "ui/base",
+    	carousel: "ui/carousel",
+    	dropdown: "ui/dropdown"
+    },
+    modules: [
+        {
+            name: 'carousel'
+        }
+    ]
+})
