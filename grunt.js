06661367874ec61f56c3f76c470c447ebd8a50fa
Mike Ball <mikedball@gmail.com>
2012-11-06 10:59:39 -0500

    Working on wiring up requirejs and grunt. WIP.

---
+++ grunt.js
@@ -0,0 +1,66 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    meta: {
+      version: '0.0.1',
+      banner: '/*! XUI - v<%= meta.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+        '* Copyright (c) <%= grunt.template.today("yyyy") %> */'
+    },
+    requirejs: {
+      std: {
+        dir: "build",
+        appDir: "lib",
+        baseUrl: "js",
+        include: "*"
+      }
+    },
+    min: {
+      xui: {
+        src: ['<banner:meta.banner>', 'lib/ui/base.js', 'lib/ui/init.js', 'lib/ui/carousel.js', 'lib/ui/dropdown.js','lib/ui/tab.js'],
+        dest: 'build/xui.min.js'
+      },
+      // some custom build examples
+      carousel: {
+        src: ['<banner:meta.banner>', 'lib/ui/carousel.js'],
+        dest: 'build/carousel.min.js'
+      },
+      carouselWithPagination: {
+        src: ['<banner:meta.banner>', 'lib/carousel.js', 'lib/addons/carousel_pagination.js'],
+        dest: 'build/carouselWithPagination.min.js'
+      }
+    },
+    lint: {
+      files: ['*.js', 'test/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    jshint: {
+      globals: {
+        exports: true
+      }
+    },
+    jasmine_node: {
+      specFolderName: "./test",
+      projectRoot: "./lib",
+      requirejs: true,
+      forceExit: true,
+      jUnit: {
+        report: false,
+        savePath : "./build/reports/jasmine/",
+        useDotNotation: true,
+        consolidate: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-requirejs');
+  grunt.loadNpmTasks('grunt-jasmine-node');
+
+  // Default task.
+  grunt.registerTask('default', 'lint requirejs jasmine_node');
+};
