8f242f923f7379c0cd99f9c5460e43511ff31f63
Mike Ball <mikedball@gmail.com>
2012-11-07 10:40:28 -0500

    Set up jasmine_node grunt config and renamed 'test' to 'spec', as is convention in jasmine.

--- grunt.js
+++ grunt.js
@@ -35,16 +35,10 @@ module.exports = function(grunt) {
       }
     },
     jasmine_node: {
-      specFolderName: "./test",
-      projectRoot: "./lib",
+      specFolderName: "",
+      projectRoot: "./spec",
       requirejs: true,
-      forceExit: true,
-      jUnit: {
-        report: false,
-        savePath : "./build/reports/jasmine/",
-        useDotNotation: true,
-        consolidate: true
-      }
+      forceExit: true
     }
   });
 
