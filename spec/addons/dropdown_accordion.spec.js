c4d81bdb29b8371722214ab7d5a21924fc4203cb
Andrew Larkin <andrewlarkin2@gmail.com>
2012-11-26 15:07:57 -0500

    Fixing bad accordion tests

--- spec/addons/dropdown_accordion.spec.js
+++ spec/addons/dropdown_accordion.spec.js
@@ -3,7 +3,7 @@ define(['jquery', 'dropdown', 'dropdown_accordion'], function($, Dropdown, Accor
         var el, d;
 
         beforeEach(function(){
-            el = $('<div data-addon="dropdown_accordion"><div data-role="dropdown-handle"></div><div data-role="dropdown-content"></div><div data-role="dropdown-handle"></div><div data-role="dropdown-content"></div><div data-role="dropdown-handle"></div><div data-role="dropdown-content"></div></div>');
+            el = $('<div data-addons="dropdown_accordion"><div data-role="dropdown-handle"></div><div data-role="dropdown-content"></div><div data-role="dropdown-handle"></div><div data-role="dropdown-content"></div><div data-role="dropdown-handle"></div><div data-role="dropdown-content"></div></div>');
         
             d = new Dropdown(el);
         });
@@ -17,9 +17,27 @@ define(['jquery', 'dropdown', 'dropdown_accordion'], function($, Dropdown, Accor
                 runs(function(){
                     spyOn(d, 'collapse');
 
-                    d.getHandle(1).trigger('dropdownExpand');
+                    expect(d.getHandle(1).hasClass('is-dropdown-active')).toBe(true);
+
+                    d.getHandle(2).trigger('dropdownExpand');
                     
                     expect(d.collapse).toHaveBeenCalledWith(1, 0);
+                });
+            });
+
+            it('does not collapse the dropdown that triggers the expansion', function(){
+                d.expand(1, 0);
+                
+                waits(0);
+
+                runs(function(){
+                    spyOn(d, 'collapse');
+
+                    expect(d.getHandle(1).hasClass('is-dropdown-active')).toBe(true);
+
+                    d.getHandle(1).trigger('dropdownExpand');
+                    
+                    expect(d.collapse).not.toHaveBeenCalled();
                 });
             });
         });
