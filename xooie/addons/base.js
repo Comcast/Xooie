30f1ec12b97f13d0dcdd3e403c6334d09c059c50
Andrew Larkin <andrewlarkin2@gmail.com>
2013-04-26 11:41:42 -0400

    Updating Xooie with garbage collection functionality. Also namespaced addon init events

--- xooie/addons/base.js
+++ xooie/addons/base.js
@@ -16,37 +16,44 @@
 
 define('xooie/addons/base', ['jquery'], function($) {
     var Base = function(name, constructor){
-        var defaultOptions = {},
-            initEvent = name.toLowerCase() + 'AddonInit',
-            className = 'has-' + name.toLowerCase() + '-addon',
+        var defaultOptions, initEvent, className, wrapper;
 
-            wrapper = function(module) {
-                //Let's check to see if module is defined...
-                //TODO: intelligently check if module is an instance of the base ui class
-                if (typeof module === 'undefined') {
-                    return false;
-                }
+        defaultOptions = {};
+        initEvent = 'xooie-addon-init.' + name.toLowerCase();
+        className = 'has-' + name.toLowerCase() + '-addon';
 
-                //if we've already instantiated an instance of this addon for this module, return it
-                if (module.addons && typeof module.addons[name] !== 'undefined') {
-                    return module.addons[name];
-                }
+        wrapper = function(module) {
+            //Let's check to see if module is defined...
+            //TODO: intelligently check if module is an instance of the base ui class
+            if (typeof module === 'undefined') {
+                return false;
+            }
 
-                //module is the module we are extending.
-                this.module = module;
+            //if we've already instantiated an instance of this addon for this module, return it
+            if (module.addons && typeof module.addons[name] !== 'undefined') {
+                return module.addons[name];
+            }
 
-                //track this addon on the parent module
-                this.module.addons[name] = this;
+            //module is the module we are extending.
+            this.module = module;
 
-                //We'll need to extend the module's base properties
-                this.options = $.extend({}, wrapper.getDefaultOptions(), this.module.options);
+            //track this addon on the parent module
+            this.module.addons[name] = this;
 
-                this.module.root.addClass(className);
+            //We'll need to extend the module's base properties
+            this.options = $.extend({}, wrapper.getDefaultOptions(), this.module.options);
 
-                constructor.apply(this, arguments);
+            this.module.root.addClass(className);
 
-                this.module.root.trigger(initEvent);
-            };
+            constructor.apply(this, arguments);
+
+            this.module.root.trigger(initEvent);
+
+        };
+
+        wrapper.prototype.cleanup = function() {
+            this.module.root.removeClass(className);
+        };
 
         wrapper.prototype.cleanup = function() {
             this.module.root.removeClass(className);
