70e161fec6d5316bbe3ab13f20ca5bab9b07a2e8
Andrew Larkin <andrewlarkin2@gmail.com>
2013-07-08 21:37:59 -0400

    Cleaning up some files

--- xooie/xooie_helpers.js
+++ xooie/xooie_helpers.js
@@ -7,6 +7,16 @@ define('xooie_helpers', ['jquery'], function($){
         } else if (typeof data === 'array') {
             return data;
         }
+    },
+
+    copyObj: function(dst, src) {
+        var name;
+
+        for (name in src) {
+            if (src.hasOwnProperty(name)) {
+                dst[name] = src[name];
+            }
+        }
     }
   };
 
