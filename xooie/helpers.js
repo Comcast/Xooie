17b5824f6f56a830a235856487d32ec4d29588e5
Andrew Larkin <andrewlarkin2@gmail.com>
2013-07-24 22:11:43 -0400

    Renames xooie_helpers -> helpers and updated module

---
+++ xooie/helpers.js
@@ -0,0 +1,27 @@
+/**
+ * class Xooie.helpers
+ *
+ * A collection of helper methods used by Xooie modules.
+ **/
+
+define('xooie/helpers', ['jquery'], function($){
+
+  var helpers = {
+/**
+ * Xooie.helpers.toAry(str) -> Array
+ * - str (String | Array): The string to be converted to an array, or an array.
+ *
+ * Converts a string to an array, or returns the passed argument if it is already an array.  Used
+ * when parsing data attributes that can be either a space-delineated string or an array.
+ **/
+    toAry: function(str) {
+        if (typeof str === 'string') {
+            return str.split(/\s+/);
+        } else if (typeof str === 'array') {
+            return str;
+        }
+    }
+  };
+
+  return helpers;
+});
