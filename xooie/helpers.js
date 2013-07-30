31799d8f4efd176df1bc56b892fc700713711185
Andrew Larkin <andrewlarkin2@gmail.com>
2013-07-30 07:21:27 -0400

    Added additional helper methods (tests pending)

--- xooie/helpers.js
+++ xooie/helpers.js
@@ -1,3 +1,19 @@
+/*
+*   Copyright 2012 Comcast
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*   Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+*/
+
 /**
  * class Xooie.helpers
  *
@@ -5,8 +21,7 @@
  **/
 
 define('xooie/helpers', ['jquery'], function($){
-
-  var helpers = {
+  var helpers = {};
 /**
  * Xooie.helpers.toAry(str) -> Array
  * - str (String | Array): The string to be converted to an array, or an array.
@@ -14,13 +29,32 @@ define('xooie/helpers', ['jquery'], function($){
  * Converts a string to an array, or returns the passed argument if it is already an array.  Used
  * when parsing data attributes that can be either a space-delineated string or an array.
  **/
-    toAry: function(str) {
-        if (typeof str === 'string') {
-            return str.split(/\s+/);
-        } else if (str instanceof Array) {
-            return str;
-        }
+  helpers.toAry = function(str) {
+    if (typeof str === 'string') {
+      return str.split(/\s+/);
+    } else if (str instanceof Array) {
+      return str;
     }
+  };
+
+  helpers.toInt = function(int) {
+    return parseInt(int, 10);
+  };
+
+  helpers.isArray = Array.isArray || function(ary) {
+    return Array.prototype.toString(ary) === '[object, Array]';
+  };
+
+  helpers.isObject = function(obj) {
+    return Object.prototype.toString(obj) === '[object, object]';
+  };
+
+  helpers.isUndefined = function(obj) {
+    return obj === void 0;
+  };
+
+  helpers.isFunction = function(func) {
+    return typeof func === 'function';
   };
 
   return helpers;
