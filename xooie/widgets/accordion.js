1cacedd8605221c89f718b14b2306d61ddba356c
Andrew Larkin <andrewlarkin2@gmail.com>
2013-08-05 08:09:45 -0400

    Fix for accordion

--- xooie/widgets/accordion.js
+++ xooie/widgets/accordion.js
@@ -2,7 +2,8 @@ define('xooie/widgets/accordion', ['jquery', 'xooie/widgets/tab'], function($, T
   var Accordion = Tab.extend(function() {
     var self = this;
 
-    this._tabEvents.clear();
+    this._tabEvents.clear('keyup');
+    this._tabEvents.clear('mouseup');
 
     this._tabEvents.add({
       keyup: function(event){
@@ -31,6 +32,8 @@ define('xooie/widgets/accordion', ['jquery', 'xooie/widgets/tab'], function($, T
     });
   });
 
+  Accordion.define('namespace', 'accordion');
+
 /** internal
  * Xooie.Accordion#_process_role_tablist(tablist) -> Element
  * - tablist (Element): A jQuery-selected collection of [[Xooie.Tab#tablists]]
@@ -38,7 +41,7 @@ define('xooie/widgets/accordion', ['jquery', 'xooie/widgets/tab'], function($, T
  * Same as [[Xooie.Tab#_process_role_tablist]] and also adds the [`aria-multiselectable="true"`](http://www.w3.org/TR/wai-aria/states_and_properties#aria-multiselectable) attribute.
  **/
   Accordion.prototype._process_role_tablist = function(tablist) {
-    Tab.prototype._process_role_tablist.apply(this, tablist);
+    Tab.prototype._process_role_tablist.apply(this, arguments);
 
     tablist.attr('aria-multiselectable', true);
 
