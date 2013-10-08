4ea85847fad430f7418271fccbfe28208cf330f6
Andrew Larkin <andrewlarkin2@gmail.com>
2013-10-08 00:16:03 -0400

    Adding ability to add properties to an existing css rule

--- spec/stylesheet.spec.js
+++ spec/stylesheet.spec.js
@@ -1,98 +1,142 @@
 require(['jquery', 'xooie/stylesheet'], function($, Stylesheet){
-    describe('Dynamic Stylesheets', function(){
+  describe('Dynamic Stylesheets', function(){
 
-        describe('When instantiating a new stylesheet...', function(){
-            var s;
+    describe('When instantiating a new stylesheet...', function(){
+      var s;
 
-            beforeEach(function(){
-                s = new Stylesheet('test');
-            });
+      beforeEach(function(){
+        s = new Stylesheet('test');
+      });
 
-            it('creates a new style object if the stylesheet does not exist', function(){
-                var initialLength = $('style').length;
+      it('creates a new style object if the stylesheet does not exist', function(){
+        var initialLength = $('style').length;
 
-                new Stylesheet('abba');
+        new Stylesheet('abba');
 
-                expect($('style').length).toBe(initialLength + 1);
-            });
+        expect($('style').length).toBe(initialLength + 1);
+      });
 
-            it('sets the stylesheet property of the instantiated stylesheet to the matching document.styleSheets object', function(){
-                expect(document.styleSheets[2].ownerNode.getAttribute('id')).toBe('test');
-                expect(document.styleSheets[2]).toBe(s.get());
-            });
+      it('sets the stylesheet property of the instantiated stylesheet to the matching document.styleSheets object', function(){
+        expect(document.styleSheets[2].ownerNode.getAttribute('id')).toBe('test');
+        expect(document.styleSheets[2]).toBe(s.get());
+      });
 
-            it('adds a title to the created stylesheet', function(){
-                expect(s.element.attr('id')).toEqual('test');
-            });
+      it('adds a title to the created stylesheet', function(){
+        expect(s.element.attr('id')).toEqual('test');
+      });
 
-            it('adds a comment to the created stylesheet indicating that this is a dynamic stylesheet', function(){
-               expect(s.element.text()).toEqual('/* This is a dynamically generated stylesheet: test */');
-            });
+      it('adds a comment to the created stylesheet indicating that this is a dynamic stylesheet', function(){
+        expect(s.element.text()).toEqual('/* This is a dynamically generated stylesheet: test */');
+      });
 
-            it('retrieves a stylesheet if one already exists', function(){
-                var element = $('<style id="testb">/* testb */</style>').appendTo($('head'));
+      it('retrieves a stylesheet if one already exists', function(){
+        var element = $('<style id="testb">/* testb */</style>').appendTo($('head'));
 
-                var s = new Stylesheet('testb');
+        var s = new Stylesheet('testb');
 
-                expect(s.element.is(element)).toBe(true);
-            });
-        });
-
-        describe('When adding a new rule...', function(){
-            var s;
-
-            beforeEach(function(){
-                s = new Stylesheet('testc');
-            });
-
-            it('adds a new rule to the end of the cssRule array', function(){
-                s.addRule('test_rule_a');
-
-                expect(s.get().cssRules[0].selectorText).toBe('test_rule_a');
-            });
-
-            it('retrieves the rule if the rule already exists', function(){
-                var rule = s.addRule('test_rule_b');
-
-                expect(s.addRule('test_rule_b')).toBe(rule);
-            });
-        });
-
-        describe('When getting a rule...', function(){
-            var s;
-            beforeEach(function(){
-                s = new Stylesheet('testd');
-            });
-
-            it('returns false if the rule does not exist', function(){
-                expect(s.getRule('test_rule_c')).toBe(false);
-            });
-
-            it('returns the rule if it exists', function(){
-                s.addRule('test_rule_c');
-
-                expect(s.getRule('test_rule_c')).not.toBe(false);
-            });
-        });
-
-        describe('When deleting a rule...', function(){
-            var s;
-
-            beforeEach(function(){
-                s = new Stylesheet('teste');
-
-                s.addRule('test_rule_d');
-            });
-
-            it('removes the rule from the stylesheet object', function(){
-                expect(s.deleteRule('test_rule_d')).toBe(true);
-
-                expect(s.getRule('test_rule_d')).toBe(false);
-            });
-
-            it('returns false if the rule does not exist', function(){
-                expect(s.deleteRule('test_rule_e')).toBe(false);
-            });
-        });
+        expect(s.element.is(element)).toBe(true);
+      });
     });
+
+    describe('When adding a new rule...', function(){
+      var s;
+
+      beforeEach(function(){
+        s = new Stylesheet('testc');
+      });
+
+      it('adds a new rule to the end of the cssRule array', function(){
+        s.addRule('test_rule_a');
+
+        expect(s.get().cssRules[0].selectorText).toBe('test_rule_a');
+      });
+
+      it('retrieves the rule if the rule already exists', function(){
+        var rule = s.addRule('test_rule_b');
+
+        expect(s.addRule('test_rule_b')).toBe(rule);
+      });
+    });
+
+    describe('When getting a rule...', function(){
+      var s;
+      beforeEach(function(){
+        s = new Stylesheet('testd');
+      });
+
+      it('returns false if the rule does not exist', function(){
+        expect(s.getRule('test_rule_c')).toBe(false);
+      });
+
+      it('returns the rule if it exists', function(){
+        s.addRule('test_rule_c');
+
+        expect(s.getRule('test_rule_c')).not.toBe(false);
+      });
+    });
+
+    describe('When adding properties of a rule...', function() {
+      var s;
+
+      beforeEach(function() {
+        s = new Stylesheet('testf');
+
+        s.addRule('test_rule_f');
+      });
+
+      it('does not add properties if the rule does not exits', function() {
+        var rule = s.getRule('test_rule_f');
+
+        spyOn(s, 'getRule').andReturn(false);
+
+        s.addProperties('test_rule_f', 'display', 'none');
+
+        expect(rule.style['display']).not.toBe('none');
+      });
+
+      it('sets a single property', function() {
+        s.addProperties('test_rule_f', 'display', 'none');
+
+        expect(s.getRule('test_rule_f').style['display']).toBe('none');
+      });
+
+      it('does not set single property if property value is undefined', function() {
+        s.getRule('test_rule_f').style['display'] = '';
+
+        s.addProperties('test_rule_f', 'display');
+
+        expect(s.getRule('test_rule_f').style['display']).toBe('');
+      });
+
+      it('adds a hash of properties', function() {
+        s.addProperties('test_rule_f', {
+          'width': '50px',
+          'height': '100px'
+        });
+
+        expect(s.getRule('test_rule_f').style['width']).toBe('50px');
+        expect(s.getRule('test_rule_f').style['height']).toBe('100px');
+      });
+    });
+
+    describe('When deleting a rule...', function(){
+      var s;
+
+      beforeEach(function(){
+        s = new Stylesheet('teste');
+
+        s.addRule('test_rule_d');
+      });
+
+      it('removes the rule from the stylesheet object', function(){
+        expect(s.deleteRule('test_rule_d')).toBe(true);
+
+        expect(s.getRule('test_rule_d')).toBe(false);
+      });
+
+      it('returns false if the rule does not exist', function(){
+        expect(s.deleteRule('test_rule_e')).toBe(false);
+      });
+    });
+  });
 });
--- xooie/stylesheet.js
+++ xooie/stylesheet.js
@@ -14,101 +14,119 @@
 *   limitations under the License.
 */
 
-define('xooie/stylesheet', ['jquery'], function($) {
-    var Stylesheet = function(name){
-        var i, title;
+define('xooie/stylesheet', ['jquery', 'xooie/helpers'], function($, helpers) {
+  var Stylesheet = function(name){
+    var i, title;
 
-        //check to see if a stylesheet already exists with this name
-        this.element = $('style[id=' + name + ']');
+    //check to see if a stylesheet already exists with this name
+    this.element = $('style[id=' + name + ']');
 
-        if (this.element.length <= 0) {
-            //if it does, use it, else create a new one
-            this.element = $(['<style id="' + name + '">',
-                                '/* This is a dynamically generated stylesheet: ' + name + ' */',
-                            '</style>'].join(''));
+    if (this.element.length <= 0) {
+      //if it does, use it, else create a new one
+      this.element = $(['<style id="' + name + '">',
+                          '/* This is a dynamically generated stylesheet: ' + name + ' */',
+                      '</style>'].join(''));
 
-            this.element.appendTo($('head'));
+      this.element.appendTo($('head'));
+    }
+
+    if (document.styleSheets) {
+      for (i = 0; i < document.styleSheets.length; i += 1){
+        if (document.styleSheets[i].ownerNode.getAttribute('id') === name) {
+          this._index = i;
+        }
+      }
+    }
+  };
+
+  Stylesheet.prototype.get = function(){
+    return document.styleSheets[this._index];
+  };
+
+  Stylesheet.prototype.getRule = function(ruleName){
+    ruleName = ruleName.toLowerCase();
+
+    var i, rules;
+
+    //Check if this uses the IE format (styleSheet.rules) or the Mozilla/Webkit format
+    rules = this.get().cssRules || this.get().rules;
+
+    for (i = 0; i < rules.length; i += 1){
+      if (rules[i].selectorText.toLowerCase() === ruleName) {
+        return rules[i];
+      }
+    }
+
+    return false;
+  };
+
+  Stylesheet.prototype.addRule = function(ruleName, properties){
+    var rule = this.getRule(ruleName), index, prop, propString = '';
+
+    if (!rule){
+      for (prop in properties) {
+        propString += prop + ': ' + properties[prop] + ';';
+      }
+
+      if (helpers.isFunction(this.get().insertRule)) {
+        //This is the W3C-preferred method
+        index = this.get().cssRules.length;
+        this.get().insertRule(ruleName + ' {' + propString + '}', index);
+        rule = this.get().cssRules[index];
+      } else {
+        //support for IE < 9
+        index = this.get().rules.length;
+        this.get().addRule(ruleName, propString, index);
+        rule = this.get().rules[index];
+      }
+    }
+
+    return rule;
+  };
+
+  Stylesheet.prototype.addProperties = function(ruleName, props, property) {
+    var rule, p;
+
+    rule = this.getRule(ruleName);
+
+    if (!rule) {
+      return;
+    }
+
+    if (helpers.isString(props) && !helpers.isUndefined(property)) {
+      rule.style[props] = property; // 
+    } else {
+      for (p in props) {
+        rule.style[p] = props[p];
+      }
+    }
+  };
+
+  Stylesheet.prototype.deleteRule = function(ruleName){
+    ruleName = ruleName.toLowerCase();
+
+    var i, rules;
+
+    //Check if this uses the IE format (styleSheet.rules) or the Mozilla/Webkit format
+    rules = this.get().cssRules || this.get().rules;
+
+    for (i = 0; i < rules.length; i += 1){
+      if (rules[i].selectorText.toLowerCase() === ruleName) {
+        if (helpers.isFunction(this.get().deleteRule)) {
+          //this is the W3C-preferred method
+          this.get().deleteRule(i);
+        } else {
+          //support for IE < 9
+          this.get().removeRule(i);
         }
 
-        if (document.styleSheets) {
-            for (i = 0; i < document.styleSheets.length; i += 1){
-                if (document.styleSheets[i].ownerNode.getAttribute('id') === name) {
-                    this._index = i;
-                }
-            }
-        }
-    };
+        return true;
+      }
+    }
 
-    Stylesheet.prototype.get = function(){
-        return document.styleSheets[this._index];
-    };
+    return false;
+  };
 
-    Stylesheet.prototype.getRule = function(ruleName){
-        ruleName = ruleName.toLowerCase();
-
-        var i, rules;
-
-        //Check if this uses the IE format (styleSheet.rules) or the Mozilla/Webkit format
-        rules = this.get().cssRules || this.get().rules;
-
-        for (i = 0; i < rules.length; i += 1){
-            if (rules[i].selectorText.toLowerCase() === ruleName) {
-                return rules[i];
-            }
-        }
-
-        return false;
-    };
-
-    Stylesheet.prototype.addRule = function(ruleName, properties){
-        var rule = this.getRule(ruleName), index, prop, propString = '';
-
-        if (!rule){
-            for (prop in properties) {
-                propString += prop + ': ' + properties[prop] + ';';
-            }
-
-            if (this.get().insertRule) {
-                //This is the W3C-preferred method
-                index = this.get().cssRules.length;
-                this.get().insertRule(ruleName + ' {' + propString + '}', index);
-                rule = this.get().cssRules[index];
-            } else {
-                //support for IE < 9
-                index = this.get().rules.length;
-                this.get().addRule(ruleName, propString, index);
-                rule = this.get().rules[index];
-            }
-        }
-
-        return rule;
-    };
-
-    Stylesheet.prototype.deleteRule = function(ruleName){
-        ruleName = ruleName.toLowerCase();
-
-        var i, rules;
-
-        //Check if this uses the IE format (styleSheet.rules) or the Mozilla/Webkit format
-        rules = this.get().cssRules || this.get().rules;
-
-        for (i = 0; i < rules.length; i += 1){
-            if (rules[i].selectorText.toLowerCase() === ruleName) {
-                if (this.get().deleteRule) {
-                    //this is the W3C-preferred method
-                    this.get().deleteRule(i);
-                } else {
-                    //support for IE < 9
-                    this.get().removeRule(i);
-                }
-
-                return true;
-            }
-        }
-
-        return false;
-    };
-
-    return Stylesheet;
+  return Stylesheet;
 
 });
