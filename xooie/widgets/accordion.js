c613212b84e085e8a2daacab538d59bc52eece75
Jeff Avallone <jeff.avallone@gmail.com>
2013-10-12 10:17:38 -0400

    Refactoring how active tabs are selected in Tab and Accordion
    Adding the selectTabs method makes it easier to customize Tab and
    Accordion functionality in derived widgets and addons.

--- xooie/widgets/accordion.js
+++ xooie/widgets/accordion.js
@@ -1,35 +1,5 @@
 define('xooie/widgets/accordion', ['jquery', 'xooie/widgets/tab'], function($, Tab){
   var Accordion = Tab.extend(function() {
-    var self = this;
-
-    this._tabEvents.clear('keyup');
-    this._tabEvents.clear('mouseup');
-
-    this._tabEvents.add({
-      keyup: function(event){
-        var activeTab = self.getActiveTabs();
-
-        if ([13,32].indexOf(event.which) !== -1){
-          if (activeTab.is(this)) {
-            self.deactivateTab(activeTab);
-          } else {
-            self.activateTab($(this));
-          }
-
-          event.preventDefault();
-        }
-      },
-
-      mouseup: function(){
-        var activeTab = self.getActiveTabs();
-
-        if (activeTab.is(this)) {
-          self.deactivateTab(activeTab);
-        } else {
-          self.activateTab($(this));
-        }
-      }
-    });
   });
 
   Accordion.define('namespace', 'accordion');
@@ -46,6 +16,16 @@ define('xooie/widgets/accordion', ['jquery', 'xooie/widgets/tab'], function($, T
     tablist.attr('aria-multiselectable', true);
 
     return tablist;
+  };
+
+  Accordion.prototype.selectTabs = function(event, selectedTab) {
+    var activeTabs = this.getActiveTabs();
+
+    if (activeTabs.is(selectedTab)) {
+      return activeTabs.not(selectedTab);
+    } else {
+      return activeTabs.add(selectedTab);
+    }
   };
 
   return Accordion;
